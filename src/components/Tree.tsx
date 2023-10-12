c693ac28cf401f70aa02ae373e64802f20cd5a01
Sergey Goremykin <gsa.fantlab@gmail.com>
2023-10-12 19:13:51 +0300

    fix tree

--- src/components/Tree.tsx
+++ src/components/Tree.tsx
@@ -3,11 +3,15 @@ import { Link } from "./Link";
 import { sumCount } from "@/utils";
 
 export const Tree = (member: IMember & { children?: IMember[] }) => {
+  const sum = sumCount(member);
   return (
     <>
       <li key={member.id}>
         <div style={{ display: "flex", gap: "16px" }}>
-          {Link(member.id)} {sumCount(member)}
+          {Link(member.id)}{" "}
+          {sum - member.count > 0
+            ? sum + " = " + member.count + " + " + (sum - member.count)
+            : sum}
         </div>
       </li>
       {!!member.children?.length && (
